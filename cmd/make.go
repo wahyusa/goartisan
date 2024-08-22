8a23dce921bde4dd94e128b210a76de49f22583f
wahyusa <wahyuskull22@gmail.com>
2024-08-22 16:07:46 +0700

    feat: :sparkles: New `goartisan make:all entityname` will generate model,repo,service, and handler

--- cmd/make.go
+++ cmd/make.go
@@ -28,9 +28,36 @@ var makeRepoCmd = createGenerateCommand("make:repo", "Generate a new repository"
 var makeServiceCmd = createGenerateCommand("make:service", "Generate a new service", generator.GenerateService)
 var makeHandlerCmd = createGenerateCommand("make:handler", "Generate a new handler", generator.GenerateHandler)
 
+var makeAllCmd = &cobra.Command{
+	Use:   "make:all [name]",
+	Short: "Generate model, repository, service, and handler",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		if err := generator.GenerateModel(name); err != nil {
+			fmt.Println("Error generating model:", err)
+			return
+		}
+		if err := generator.GenerateRepository(name); err != nil {
+			fmt.Println("Error generating repository:", err)
+			return
+		}
+		if err := generator.GenerateService(name); err != nil {
+			fmt.Println("Error generating service:", err)
+			return
+		}
+		if err := generator.GenerateHandler(name); err != nil {
+			fmt.Println("Error generating handler:", err)
+			return
+		}
+		fmt.Println("All components generated successfully:", name)
+	},
+}
+
 func init() {
 	baseCmd.AddCommand(makeModelCmd)
 	baseCmd.AddCommand(makeRepoCmd)
 	baseCmd.AddCommand(makeServiceCmd)
 	baseCmd.AddCommand(makeHandlerCmd)
+	baseCmd.AddCommand(makeAllCmd)
 }
